c0a3daba3654036e8a74bfd50115cc9eaa902faa
talita-santos <talita.os@hotmail.com>
2019-06-21 15:46:44 -0300

    responsivo da nav, banner e ecossistema

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+    $(function () {
+        $('.dropdown-toggle').dropdown();
+    });
+});
+
+//     $('.menuHamburguer').click(function (event) {
+//         $('.menuDropdown').slideToggle();
+//     });
+
+//     // scroll suave para seções a partir do menu hamburguer
+//     $('.menuDropdown--link').click(function (event) {
+//         $('.menuDropdown').slideToggle();
+//         if (this.hash !== "") {
+//             let hash = this.hash;
+//             $('html, body').animate({
+//                 scrollTop: $(hash).offset().top
+//             }, 800, function () {
+//                 window.location.hash = hash;
+//             });
+//         }
+//     });  //nav fixa com transição da logo
+
+
+
+// });
